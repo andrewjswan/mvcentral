1beb58ec9ec59200b685f61ec6c96acd190d6e79
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-03-27 16:45:48 +0200

    Rework MPE creation

--- scripts/Create_Installer.bat
+++ scripts/Create_Installer.bat
@@ -18,23 +18,8 @@ FOR /F "tokens=*" %%i IN ('Tools\sigcheck.exe /accepteula /nobanner /n "..\mvCen
 :: Temp xmp2 file
 copy ..\MPEI\mvCentral.xmp2 ..\MPEI\mvCentralTemp.xmp2
 
-:: Sed "{VERSION}" from xmp2 file
-Tools\sed.exe -i "s/{VERSION}/%version%/g" ..\MPEI\mvCentralTemp.xmp2
-
 :: Build MPE1
 "%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" ..\MPEI\mvCentralTemp.xmp2 /B /V=%version% /UpdateXML
 
 :: Cleanup
 del ..\MPEI\mvCentralTemp.xmp2
-
-:: Parse version (Might be needed in the futute)
-FOR /F "tokens=1-4 delims=." %%i IN ("%version%") DO ( 
-    SET major=%%i
-    SET minor=%%j
-    SET build=%%k
-    SET revision=%%l
-)
-
-:: Rename MPE1
-if exist "..\MPEI\Release\mvCentral-%major%.%minor%.%build%.%revision%.mpe1" del "..\MPEI\Release\mvCentral-%major%.%minor%.%build%.%revision%.mpe1"
-rename ..\MPEI\Release\mvCentral-MAJOR.MINOR.BUILD.REVISION.mpe1 "mvCentral-%major%.%minor%.%build%.%revision%.mpe1"
